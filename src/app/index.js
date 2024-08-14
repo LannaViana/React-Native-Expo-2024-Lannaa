3a73a55acac3741927d73cb7b9633bb93c5fde63
Lanna Gabriela <166149007+LannaViana@users.noreply.github.com>
2024-08-14 14:09:21 +0000

    [fade] aula 4

--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { useAuth } from '../hooks/Auth';
 
 export default function App() {
+  const { singIn, singOut } = useAuth();
+
   return (
     <View style={styles.container}>
       <Text style={styles.tittle}>Aplicativo pronto para usar</Text>
+      <Button
+       title="Singin Super" 
+       onPress={()=>
+       singIn({email: "super@email.com", password: "Super123!"})
+       } 
+       />
+       <Button
+       title="Singin Adm" 
+       onPress={()=>
+       singIn({email: "adm@email.com", password: "Adm123!"})
+       } 
+       />
+       <Button
+       title="Singin User" 
+       onPress={()=>
+       singIn({email: "user@email.com", password: "User123!"})
+       } 
+       />
+      <Button title="Singout" onPress={() => signOut()} />
       <StatusBar style="auto" />
     </View>
   );
