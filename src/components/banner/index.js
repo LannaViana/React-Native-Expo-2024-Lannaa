4d65cbd0e84bb7d4d9b84448648de97dba0ce9a0
Lanna Gabriela <166149007+LannaViana@users.noreply.github.com>
2024-10-23 11:42:10 +0000

    [feat] aula 10 7;00

--- src/components/banner/index.js
+++ src/components/banner/index.js
@@ -7,8 +7,14 @@ export function Banner() {
     return (
         <View style={styles.container}>
            <PagerView>
-              <View>
-                  <Text>Banner</Text>
+              <View key="1">
+                  <Text>Banner 1</Text>
+              </View>
+              <View key="2">
+                  <Text>Banner 2</Text>
+              </View>
+              <View key="3">
+                  <Text>Banner 3</Text>
               </View>
            </PagerView>
         </View>
